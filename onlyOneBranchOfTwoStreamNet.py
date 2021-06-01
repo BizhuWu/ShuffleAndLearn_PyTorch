e642c955d5ac70d85d2dc5c0f44d30c46a9beac0
BizhuWu <905705049@qq.com>
2021-06-01 19:49:28 +0800

    Update onlyOneBranchOfTwoStreamNet.py

--- onlyOneBranchOfTwoStreamNet.py
+++ onlyOneBranchOfTwoStreamNet.py
@@ -6,10 +6,6 @@ import torch
 class oneStreamNet(nn.Module):
     def __init__(self):
         super(oneStreamNet, self).__init__()
-        # self.branch = models.alexnet()
-        # # fc8
-        # self.branch.classifier[6] = nn.Linear(in_features=4096, out_features=101, bias=True)
-
         self.branch = models.resnet18()
         self.branch.fc = nn.Linear(in_features=512, out_features=101, bias=True)
 
