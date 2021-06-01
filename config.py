557f832bfc72b4859a9a9acd6759465158cd4e7a
BizhuWu <905705049@qq.com>
2021-06-01 19:46:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-UCF101_Dataset_root = '/data/wubizhu/dataset/UCF101/UCF-101/'
-# UCF101_Dataset_root = 'data/UCF101/UCF-101/'
+UCF101_Dataset_root = 'data/UCF101/UCF-101/'
 SSL_epoch = 1
 SSL_lr = 0.0001
 SSL_batch_size = 256
@@ -7,8 +6,7 @@ SSL_batch_size = 256
 
 
 
-UCF101_original_Dataset_root = '/data/wubizhu/dataset/UCF101/UCF-101-original/'
-# UCF101_original_Dataset_root = 'data/UCF101/UCF-101-original/'
+UCF101_original_Dataset_root = 'data/UCF101/UCF-101-original/'
 AR_lr = 0.1
 AR_epoch = 50
 AR_TRAIN_BATCH_SIZE = 512
